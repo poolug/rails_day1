60bccdc576b456189ab221dd962e946ed0caa893
poolug <pauluzcategui43@gmail.com>
2021-07-02 00:18:28 -0400

    update file carta.rb

--- carta.rb
+++ carta.rb
@@ -1,10 +1,10 @@
 class Card
     attr_accessor :number, :kind
     def initialize(number, kind)
-        if (number >= 1 || number <= 13) && ["C", "D", "T", "E"].include?(kind)
+        @number = number
+        @kind = kind
         
-            @number = number
-            @kind = kind
+        if (number >= 1 || number <= 13) && ["C", "D", "T", "E"].include?(kind)
         else
             raise "Nada que ver"
         end
--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -2,6 +2,10 @@ class BankAccount
     attr_accessor :name_user
     attr_reader :vip, :number_account
     def initialize(name_user, number_account, vip = 0)
+        @name_user = name_user
+        @number_account = number_account
+        @vip = vip
+        
         if number_account.digits.count != 8
             raise RangeError, "Número de dígitos incorrecto"
         end
@@ -9,9 +13,6 @@ class BankAccount
         if !(0..1).include? (vip)
             raise RangeError, "Número VIP incorrecto"
         end
-        @name_user = name_user
-        @number_account = number_account
-        @vip = vip
     end
     def account_vip
         "#{self.vip}-#{self.number_account}"
