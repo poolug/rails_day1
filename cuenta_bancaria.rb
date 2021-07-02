a670448bfb8434c1a59c7c96d71166c427eace53
poolug <pauluzcategui43@gmail.com>
2021-07-02 00:16:28 -0400

    update files

--- cuenta_bancaria.rb
+++ cuenta_bancaria.rb
@@ -1,8 +1,8 @@
 class BankAccount
     attr_accessor :name_user
-    attr_reader :vip, :number_acount
-    def initialize(name_user, number_acount, vip = 0)
-        if number_acount.digits.count != 8
+    attr_reader :vip, :number_account
+    def initialize(name_user, number_account, vip = 0)
+        if number_account.digits.count != 8
             raise RangeError, "Número de dígitos incorrecto"
         end
 
@@ -10,17 +10,14 @@ class BankAccount
             raise RangeError, "Número VIP incorrecto"
         end
         @name_user = name_user
-        @number_acount = number_acount
+        @number_account = number_account
         @vip = vip
     end
     def account_vip
-        "#{self.vip}-#{self.number_acount}"
+        "#{self.vip}-#{self.number_account}"
     end
 end
 
 results = BankAccount.new("Marcos", 87654321, 1)
-
-# puts results.name_user
-# puts results.acount_vip
 
 puts "Usuario: #{results.name_user}\nCuenta: #{results.account_vip}"
